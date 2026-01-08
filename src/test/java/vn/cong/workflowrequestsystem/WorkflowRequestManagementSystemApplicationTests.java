09f8a68b66ad52221eab380703b1a7b49c25b02c
trancong120104 <trancong120104@gmail.com>
2026-01-08 10:17:00 +0700

    init project

---
+++ src/test/java/vn/cong/workflowrequestsystem/WorkflowRequestManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.cong.workflowrequestsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowRequestManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
